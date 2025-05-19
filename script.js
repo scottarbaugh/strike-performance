345104d9d69aee2b7d9d6da6957ca8fa31e33c0d
Scott Arbaugh <scottarbaugh@gmail.com>
2025-05-19 13:40:16 -0500

    better shadow under price banner

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Main JavaScript file for Strike BTC Performance Analyzer
 
 // App version for cache busting
-const APP_VERSION = '1.0.6';
+const APP_VERSION = '1.0.7';
 
 // SUPPORTED_CURRENCIES is loaded from currencies.js
 
